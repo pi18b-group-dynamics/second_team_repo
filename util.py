d27c5b897e9d5a7675374286a1ae06159fe25e28
antipups <nikitakurkurin@protonmail.ch>
2020-02-07 13:24:02 +0300

    Создана простейшая проверка на ввод.

--- util.py
+++ util.py
@@ -14,4 +14,21 @@ def add(first_name, second_name, third_name, series, number_, sex, whos_give, da
     conn.close()
 
 
-add('Имя', 'Фамилия', 'Отчество', 'Серия', 123, 'Пол', 'Кто выдал', 'Дата выдачи')
+def check_all(first_name, second_name, third_name, series, number_, sex, whos_give, date_of_give):
+    if any(map(str.isdigit, first_name)):
+        return "Введите нормальное ИМЯ"
+    if any(map(str.isdigit, second_name)):
+        return "Введите нормальную ФАМИЛИЮ"
+    if any(map(str.isdigit, third_name)):
+        return "Введите нормальное ОТЧЕСТВО"
+    if str(type(number_)) != "<class 'int'>":
+        return "Введите нормальный НОМЕР"
+    if any(map(str.isdigit, sex)):
+        return "Введите нормальный ПОЛ"
+    if any(map(str.isdigit, whos_give)):
+        return "Введите нормальное ИЗДАНИЕ"
+    if any(map(str.isalpha, date_of_give)):
+        return "Введите нормальный ДАТУ"
+
+
+print(check_all('qwe', 'Фамилия', 'Отчество', 'Серия', 123, 'Пол', 'Кто выдал', '25.01.2020'))
