45092dc9e99808eef0f8d50d3ef96386baee45dd
antipups <nikitakurkurin@protonmail.ch>
2020-02-07 09:21:00 +0300

    Сделана главная таблица.

--- util.py
+++ util.py
@@ -5,7 +5,13 @@ conn = sqlite3.connect('Chinook_Sqlite.sqlite')     # подключение к 
 cursor = conn.cursor()
 
 
-cursor.execute('INSERT INTO test (first_name, last_name) VALUES ("Никита", "Куркурин")')
-conn.commit()
-conn.close()
+def add(first_name, second_name, third_name, series, number_, sex, whos_give, date_of_give):
+    cursor.execute('INSERT INTO passports (first_name, last_name, patronymic, series, '
+                   'number_, sex, whos_give, date_of_give) VALUES '
+                   '("{}", "{}", "{}", "{}", {}, "{}", "{}", "{}")'.format
+                   (first_name, second_name, third_name, series, number_, sex, whos_give, date_of_give))
+    conn.commit()
+    conn.close()
 
+
+add('Имя', 'Фамилия', 'Отчество', 'Серия', 123, 'Пол', 'Кто выдал', 'Дата выдачи')
