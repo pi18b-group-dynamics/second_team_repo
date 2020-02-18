7de72baaebdd5eea1e479ad9d44e702bb629a124
antipups <nikitakurkurin@protonmail.ch>
2020-02-18 14:58:32 +0300

    Сделал добавление, нужно доделать визуал / проверки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+import util
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -8,30 +9,67 @@ from kivy.uix.widget import Widget
 
 class MyApp(App):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.main_layout = BoxLayout(orientation='horizontal')
+
     def build(self):
-        main_layout = BoxLayout(orientation='horizontal')
 
-        record_layout = BoxLayout(orientation='vertical')
-        find_layout = BoxLayout(orientation='vertical')
+        record_layout = BoxLayout(orientation='vertical',
+                                  id='record')
         find_area = BoxLayout(orientation='vertical')
+        watch_area = BoxLayout(orientation='vertical')
 
-        list_of_data = 'Введите фамилию', 'Введите имя', 'Введите отчество', 'Серия', \
-                       'Номер', 'Пол', 'Кто выдал', 'Дата выдачи', 'Фотография'
+        list_of_data = {'Введите фамилию': 'last_name', 'Введите имя': 'first_name', 'Введите отчество': 'patronymic', 'Серия': 'series', \
+                       'Номер': 'number_', 'Пол': 'sex', 'Кто выдал': 'whos_give', 'Дата выдачи': 'date_of_give', 'Фотография': 'photo'}
 
-        for row in list_of_data:
-            record_layout.add_widget(TextInput(hint_text=row,
-                                               id='Добавление - ' + row))
+        record_layout.add_widget(TextInput(text='Введите данные',
+                                           readonly=True,
+                                           ))
+        for key, value in list_of_data.items():
+            record_layout.add_widget(TextInput(hint_text=key,
+                                               id='Добавление - ' + value))
+        else:
+            for button in 'Добавить', 'Удалить', 'Изменить', 'Найти':
+                record_layout.add_widget(Button(text=button,
+                                                on_press=self.work_with_db))
 
-        find_layout.add_widget(TextInput(text='Поиск',
-                                         readonly=True))
-        for row in list_of_data:
-            find_layout.add_widget(TextInput(hint_text=row,
-                                             id='Поиск - ' + row))
+        self.main_layout.add_widget(record_layout)
+        self.main_layout.add_widget(find_area)
+        self.main_layout.add_widget(watch_area)
+        return self.main_layout
 
-        main_layout.add_widget(record_layout)
-        main_layout.add_widget(find_layout)
-        main_layout.add_widget(find_area)
-        return main_layout
+    def work_with_db(self, instance):   # функция работы кнопок удалить добавить и т.д.
+        if instance.text == 'Добавить':
+            self.add()
+        elif instance.text == 'Удалить':
+            self.remove()
+        elif instance.text == 'Изменить':
+            self.update()
+        elif instance.text == 'Поиск':
+            self.search()
+
+    def add(self):  # ДОБАВЛЕНИЕ (добавить визуала лучше, и проверки !!!!)
+        # след. цикл и вообще конструкция - добираемся до input'ов
+        dict_of_data = {}   # словарь с данными полей, который мы и будем передавать
+        for box_layout in self.main_layout.children:    # цикл на перебор всех макетов (тут нам нужен только рекорд)
+            if box_layout.id == 'record':   # находим рекорд
+                for widget in box_layout.children[5:-1]:
+                    key, value = widget.id, widget.text
+                    dict_of_data.update({key[key.find('- ') + 2:]: value})
+                    widget.text = ''    # очистка поля после ввода (подумай, мб очищать только плохие поля, или все если верно)
+
+        print(util.add_in_db(**dict_of_data))
+
+    def remove(self):
+        pass
+
+    def update(self):
+        pass
+
+    def search(self):
+        util.find_in_db()
+
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -5,27 +5,39 @@ conn = sqlite3.connect('Chinook_Sqlite.sqlite')     # подключение к 
 cursor = conn.cursor()
 
 
-def check_all(first_name, second_name, third_name, series, number_, sex, whos_give, date_of_give):
-    if any(map(str.isdigit, first_name)):
+def add_in_db(**kwargs):
+    if any(map(str.isdigit, kwargs.get('first_name'))):
         return "Введите нормальное ИМЯ"
-    if any(map(str.isdigit, second_name)):
+    if any(map(str.isdigit, kwargs.get('last_name'))):
         return "Введите нормальную ФАМИЛИЮ"
-    if any(map(str.isdigit, third_name)):
+    if any(map(str.isdigit, kwargs.get('patronymic'))):
         return "Введите нормальное ОТЧЕСТВО"
-    if str(type(number_)) != "<class 'int'>":
+    if not kwargs.get('number_').isdigit():
         return "Введите нормальный НОМЕР"
-    if any(map(str.isdigit, sex)):
+    if any(map(str.isdigit, kwargs.get('sex'))):
         return "Введите нормальный ПОЛ"
-    if any(map(str.isdigit, whos_give)):
+    if any(map(str.isdigit, kwargs.get('whos_give'))):
         return "Введите нормальное ИЗДАНИЕ"
-    if any(map(str.isalpha, date_of_give)):
+    if any(map(str.isalpha, kwargs.get('date_of_give'))):
         return "Введите нормальный ДАТУ"
     cursor.execute('INSERT INTO passports (first_name, last_name, patronymic, series, '
                    'number_, sex, whos_give, date_of_give) VALUES '
                    '("{}", "{}", "{}", "{}", {}, "{}", "{}", "{}")'.format
-                   (first_name, second_name, third_name, series, number_, sex, whos_give, date_of_give))
+                   (kwargs.get('first_name'), kwargs.get('last_name'), kwargs.get('patronymic'), kwargs.get('series'),
+                    kwargs.get('number_'), kwargs.get('sex'), kwargs.get('whos_give'), kwargs.get('date_of_give')))
     conn.commit()
-    conn.close()
 
 
-print(check_all('Урод', 'Фамилия', 'Отчество', 'Серия', 'asd', 'Пол', 'Кто выдал', '25.01.2020'))
+
+def find_in_db(**kwargs):
+    string = 'SELECT * FROM passports WHERE '
+    for key, value in kwargs.items():
+        string += f'{key} = "{value}" AND '
+    else:
+        string = string[:string.rfind(' AND ')]     # режем строку так как там появился лишний _AND_
+    cursor.execute(string)
+    print(cursor.fetchall())
+
+
+# print(add_in_db('Урод', 'Фамилия', 'Отчество', 'Серия', 'asd', 'Пол', 'Кто выдал', '25.01.2020'))
+print(find_in_db(last_name='Фамилия', first_name='Урод'))
